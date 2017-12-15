b685b807a8dd51e1d74b55754b4e20a54c7ff31e
Peter Crady <petercrady1@gmail.com>
2017-12-14 21:10:22 -0600

    make pep8 and pep256 compliant

--- script.py
+++ script.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Reformating a set of downloaded csvs."""
+
 import pandas as pd
 import numpy as np
 import urllib
@@ -12,61 +14,69 @@ import sys
 pd.set_option('max_colwidth', 2000)
 pd.set_option('display.width', 20000)
 BASE_URL = 'https://s3-us-west-2.amazonaws.com/cauldron-workshop/data/'
+OUTPUT_FILE = 'output.csv'
 
-# Generates the file names a.csv, b.csv ....
+
 def generate_file_names():
-    return  [letter + '.csv' for letter in list(string.ascii_lowercase)]
+    """Return a list of filenames."""
+    return [letter + '.csv' for letter in list(string.ascii_lowercase)]
 
-# Downloads a single file given by the url
+
 def download_file(url):
-    with urllib.request.urlopen(url) as url_response:
-        downloaded_file = url_response.read().decode('utf-8')
-        with open(url.split('/')[-1], 'w') as f:
-            f.write(downloaded_file)
-            
-# Downloads multiple files in parallel. Must use multiprocessing here
-# because threading isnt really threading in python.
-# Note: would be more performant if wasnt writing the files to disk.
-def download_files(base_url):
-    urls = [base_url + name for name in generate_file_names()]
-    pool = multiprocessing.Pool(processes=26)
-    pool.map(download_file, urls)
+    """Return a downloaded file as a pandas dataframe."""
+    return pd.read_csv(url)
 
-# Concatinates all the dataframes from the downloaded csvs 
-def concatinate_dataframes():
-    file_names = generate_file_names()
-    df_list = [pd.read_csv(file_name) for file_name in file_names]
+
+def download_files(base_url=BASE_URL):
+    """Return a list of pandas dataframes.
+
+    Keyword arguments:
+    base_url -- the base url for downloading a list of files
+    """
+    urls = [base_url + name for name in generate_file_names()]
+    pool = multiprocessing.Pool(processes=len(urls))
+    return pool.map(download_file, urls)
+
+
+def concatinate_dataframes(df_list):
+    """Concatinate a list of dataframes into one dataframe."""
     return pd.concat(df_list)
 
-# This is like several hundred times faster. I knew there was a function
-# for this I just didnt know what it was called.
+
 def transform_dataframe(df):
-    df = df[['user_id', 'path','length']]
+    """Return a transformed pandas dataframe."""
+    df = df[['user_id', 'path', 'length']]
     return df.pivot(index="user_id", columns="path", values="length").fillna(0)
 
-# clean up downloaded files
-def delete_files():
-    for f in generate_file_names():
-        os.remove(f)
 
-# Helper function to make argprase easier
-def main_wrapper():
-    df = concatinate_dataframes()
+def write_csv(df_list, output_file="output.csv"):
+    """Write a formated csv to disk.
+
+    Keyword arguments:
+    output_file -- the name of the csv file to be written to disk
+    """
+    df = concatinate_dataframes(df_list)
     df = transform_dataframe(df)
-    df.to_csv("output.csv")
-    delete_files()
+    df.to_csv(output_file)
+
+
+def get_args():
+    """Return argparse arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--output", dest="output_file",
+                        default=OUTPUT_FILE, help="Name of the output file")
+    parser.add_argument("-u", "--url", dest="base_url",
+                        default=BASE_URL,
+                        help="Base url for downloading files")
+    return parser.parse_args()
+
 
 # Main function with a really crappy commandline argument parser.
 def main():
-    if len(sys.argv) == 2:
-        download_files(sys.argv[1])
-    elif len(sys.argv) == 1:
-        download_files(BASE_URL)
-    else:
-        return
- 
-    main_wrapper()
-
+    """The main function."""
+    args = get_args()
+    files = download_files(args.base_url)
+    write_csv(files, args.output_file)
 
 
 if __name__ == "__main__":
